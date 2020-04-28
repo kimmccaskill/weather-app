73d0dae7d23d47b0cba2e9f00d2ac6773d961f0c
Kim McCaskill <kimmccaskill92@gmail.com>
2020-04-27 21:27:55 -0600

    Refactor including object destructuring

--- utils/forecast.js
+++ utils/forecast.js
@@ -3,15 +3,13 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=a1f93f99215f7080692e1d88d3d76c43&query=${latitude},${longitude}&units=f`
 
-  request({ url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback('Unable to connect to weather service')
-    } else if (response.body.error) {
+    } else if (body.error) {
       callback('Unable to find location')
     } else {
-      const currentTemp = response.body.current.temperature
-      const feelsLike = response.body.current.feelslike
-      callback(undefined, response.body.current.weather_descriptions[0] + `. It is currently ${currentTemp} degrees and feels like ${feelsLike}`)
+      callback(undefined, body.current.weather_descriptions[0] + `. It is currently ${body.current.temperature} degrees and feels like ${body.current.feelslike}`)
     }
   })
 }
